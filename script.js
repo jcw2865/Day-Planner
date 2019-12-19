b0a18a28f44575eac2da1d2f703b2bc281dfbdf2
Jake Walker <jcw2865@gmail.com>
2019-12-19 17:36:27 -0600

    Added local time and date to page header using moment.js.

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log(moment.HTML5_FMT.DATE);
+console.log(moment());
+
+var currentDay = $("#currentDay").text(moment());
+// var currentDay = $("#currentDay").text(moment().format(moment.HTML5_FMT.DATE));
+
